c3d653678acdce758835ffe8f8fa43a8cf9a6319
ajay475354 <ajay475354>
2022-08-20 15:06:05 +0000

    date added

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 app.get("/", (request, response) => {
   response.send("hello world!");
-  console.log(response);
+});
+
+app.get("/date", (request, response) => {
+  let date = new Date();
+  response.send(`Today's date is ${date}`);
 });
 
 app.listen(3000);
