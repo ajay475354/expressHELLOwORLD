a0821ea3d0b8c1471be9faaa81f6acc716513f16
ajay475354 <ajay475354>
2022-08-20 15:15:19 +0000

    htmlpage

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ app.get("/date", (request, response) => {
   response.send(`Today's date is ${date}`);
 });
 
+app.get("/page", (request, response) => {
+  response.sendFile("./page.html", { root: __dirname });
+});
+
 app.listen(3000);
